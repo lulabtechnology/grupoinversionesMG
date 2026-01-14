e096bd52fa41dbee5e019cf5f7689f90d08f920a
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2026-01-14 14:44:02 -0500

    Create label.tsx

---
+++ components/ui/label.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+export function Label({ className, ...props }: LabelProps) {
+  return (
+    <label
+      className={cn("text-sm font-medium text-slate-800", className)}
+      {...props}
+    />
+  );
+}
